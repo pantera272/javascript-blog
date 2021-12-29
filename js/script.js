0962a7daaef3a5c3ff80adfbaabcfa9db79dbeb5
Krzysztof Kosek <krzysiek.kosek@gmail.com>
2021-12-29 09:35:16 +0100

    Add list tags and authors

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ document.getElementById('test-button').addEventListener('click', function(){
   console.log('links:', links);
 });
 */
-/* generate title links */
+/*function generate title links */
 function generateTitleLinks(customSelector = ''){
 	
   /*clear links*/
@@ -62,10 +62,48 @@ const titleClickHandler = function(event){
   articleSelector.classList.add('active');
 };
 
+//function find min and max values of tags 
+function calculateTagsParams(tags){
+  let min = 0;
+  let max = 0;
+  for(let tag in tags){
+    /*find max value*/
+    if(tags[tag] > max){
+      max = tags[tag];
+    }
+    /*find min value*/
+    if(min == 0){
+      min = tags[tag];
+    } else if(tags[tag] < min){
+      min = tags[tag];
+    }
+  }
 
-//generate tags function
-function generateTags()
-{
+  const tagParams = {'min': min, 'max' : max};
+  return tagParams;
+}
+
+//function generate size tag
+function calculateTagClass(count, params){
+  const optCloudClassCount = 5;
+
+  const min = params.min;
+  const max = params.max;
+  const range = max - min;
+  let percentage = count - min;
+  percentage = percentage/range;
+  const size = Math.floor(percentage*(optCloudClassCount - 1) +1);
+  return size;
+}
+
+
+//function generate tags 
+function generateTags(){
+  const optTagListerSelector = '.tags.list';
+  const optCloudClassPrefix = 'tag-size-';
+  
+  /* create a new variable allTags with an empty object */
+  let allTags = {};
   /* find all articles */
   const articles = document.querySelectorAll('article');
 
@@ -79,10 +117,33 @@ function generateTags()
     
     /* write article tag */
     for(let tag of tagArray){
-      let tagDisplay = '<li><a href="#tag-'+tag+'">'+tag+'</a></li>';
+      let tagDisplay = '<li><a href="#tag-' + tag + '"> ' + tag + ' </a></li>';
       document.querySelector('#' + articleId + ' > .post-tags .list').innerHTML += tagDisplay;
+      
+      if(!allTags[tag]){
+        allTags[tag] = 1;
+      } else {
+        allTags[tag]++;
+      }
     }
   }
+
+  const tagList = document.querySelector(optTagListerSelector);
+  
+  const tagsParams = calculateTagsParams(allTags);
+
+  /* [NEW] create variable for all links HTML code */
+  let allTagsHTML = '';
+
+  /* [NEW] START LOOP: for each tag in allTags: */
+  for(let tag in allTags){
+    let tagSize = calculateTagClass(allTags[tag], tagsParams);
+    allTagsHTML += '<li><a class="'+ optCloudClassPrefix + tagSize +'" href="#tag-' + tag + '">' + tag +'</a> (' + allTags[tag] + ')</li>';
+  }
+  /* [NEW] END LOOP: for each tag in allTags: */
+
+  /*[NEW] add HTML from allTagsHTML to tagList */
+  tagList.innerHTML = allTagsHTML;
 }
 
 function tagClickHandler(event){
@@ -165,12 +226,51 @@ function authorClickHandler(event){
   console.log(author);
 }
 
+/* generate author list*/
+function generateAuthorList()
+{
+  /*generate author list*/
+  const articles = document.querySelectorAll('article');
+  let authorList = [];
+
+  for(let article of articles){
+    const author = article.getAttribute('data-author');
+
+    if(authorList == ''){
+      authorList.push(author);
+    } else {
+      let authorIs = authorList.indexOf(author);
+      
+      if(authorIs == -1){
+        authorList.push(author);
+      }
+    }
+  }
+
+  /*generate author html link*/
+  const authorPlace = document.querySelector('.section-author');
+  
+  for(let author of authorList){
+    let articleNumber = 0;
+    for(let article of articles){
+      const authorNumber = article.getAttribute('data-author');
+      if(author == authorNumber){
+        articleNumber++;
+      } 
+    }
+    const authorHref = author.replace(' ','&');
+    authorPlace.innerHTML+= '<li><a href="#author-' + authorHref + '">' + author + '</a> (' + articleNumber + ')</li>';
+  }
+}
+
 
 //start function on page load
 generateTitleLinks();
 generateTags();
 addClickListenersToTags();
 generateAuthor();
+generateAuthorList();
 addClickListenersToAuthor();
+
 
 
